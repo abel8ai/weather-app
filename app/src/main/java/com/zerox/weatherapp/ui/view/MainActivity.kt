a95eccc22e05c2d096aff7583a007b2ffbd210c1
Abel <abochoa91@gmail.com>
2022-08-30 11:14:06 -0400

    set view binding for MainActivity

--- app/src/main/java/com/zerox/weatherapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/zerox/weatherapp/ui/view/MainActivity.kt
@@ -2,13 +2,15 @@ package com.zerox.weatherapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.zerox.weatherapp.R
+import com.zerox.weatherapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
